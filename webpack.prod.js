5095cbd5e9111521903bc59705cb01e91e5ddd97
ndrk <ndrk@nzxt>
2024-04-21 11:10:26 +0200

    Change entrypoint

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,12 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 const TerserPlugin = require("terser-webpack-plugin");
 const path = require("path");
 
 module.exports = merge(common, {
   mode: "production",
   entry: {
-    fxengine: "./src/Fxengine.js"
+    fxengine: "./src/index.js",
   },
   output: {
     filename: "[name].bundle.js",
@@ -14,12 +14,14 @@ module.exports = merge(common, {
     clean: true,
   },
   optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin({
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
         minify: TerserPlugin.uglifyJsMinify,
         // `terserOptions` options will be passed to `uglify-js`
         // Link to options - https://github.com/mishoo/UglifyJS#minify-options
         terserOptions: {},
-      })],
+      }),
+    ],
   },
 });
