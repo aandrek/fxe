3c188391c25f962995a89243496aee1c7cbb1594
ndrk <ndrk@nzxt>
2024-04-22 12:40:20 +0200

    New version. Fix in webpack config for exporting as module.

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,24 +4,27 @@ const TerserPlugin = require("terser-webpack-plugin");
 const path = require("path");
 
 module.exports = merge(common, {
-  mode: "production",
-  entry: {
-    fxengine: "./src/index.ts",
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "./dist"),
-    clean: true,
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        minify: TerserPlugin.uglifyJsMinify,
-        // `terserOptions` options will be passed to `uglify-js`
-        // Link to options - https://github.com/mishoo/UglifyJS#minify-options
-        terserOptions: {},
-      }),
-    ],
-  },
+    mode: "production",
+    entry: {
+        fxengine: "./src/Fxengine.ts",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "./dist"),
+        clean: true,
+        library: {
+            type: "module", // Indicate ESM output
+        },
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                minify: TerserPlugin.uglifyJsMinify,
+                // `terserOptions` options will be passed to `uglify-js`
+                // Link to options - https://github.com/mishoo/UglifyJS#minify-options
+                terserOptions: {},
+            }),
+        ],
+    },
 });
